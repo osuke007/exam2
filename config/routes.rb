3b7b63fca5f213b0e0a6052c635d87776f7006b1
osuke <osuke@TOKC02LQEXNFD57.local>
2017-02-05 17:22:46 +0900

    LetterOpenerの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   devise_for :users
   root to: 'top#index'
   resources :products
